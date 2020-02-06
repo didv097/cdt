be174e0f5d67a6a282a6abfd152d0144ab7c0a94
bran0 <bran107@outlook.com>
2020-02-06 14:14:04 -0500

    company files

--- src/views/dashboard/Companies/view/Documents/View.vue
+++ src/views/dashboard/Companies/view/Documents/View.vue
@@ -1,17 +1,29 @@
 <template>
   <v-container>
+    <input
+      ref="file"
+      type="file"
+      class="d-none"
+      @change="uploadFile"
+    >
     <base-material-card
       color="primary"
       icon="mdi-file"
       inline
-      :loading="loading"
     >
       <template v-slot:after-heading>
         <div class="display-2">
           Company Files / {{ directory.name }}
         </div>
       </template>
-      <v-file-input>asf</v-file-input>
+      <v-btn
+        color="secondary"
+        class="file-upload"
+        :loading="uploadingFile"
+        @click="$refs.file.click()"
+      >
+        Upload a file
+      </v-btn>
       <v-data-table
         :headers="headers"
         :items="files"
@@ -54,6 +66,7 @@
                     icon
                     color="error"
                     v-on="on"
+                    @click="fileToDelete = file.item.name, deleteMsg = true"
                   >
                     <v-icon size="24">
                       mdi-delete
@@ -67,6 +80,46 @@
         </template>
       </v-data-table>
     </base-material-card>
+    <v-dialog
+      v-model="deleteMsg"
+      persistent
+      max-width="500"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          You are about to delete a file
+        </v-card-title>
+        <v-card-text>
+          Please confirm that you would like to delete the following file: <b>{{ fileToDelete }}</b>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            text
+            @click="deleteMsg = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="error"
+            text
+            @click="deleteFile"
+          >
+            Delete File
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <base-material-snackbar
+      v-model="snackbar"
+      :color="snackbarColor"
+      bottom
+      right
+      :type="null"
+    >
+      {{ snackbarText }}
+    </base-material-snackbar>
   </v-container>
 </template>
 
@@ -93,8 +146,14 @@
           },
         ],
         loading: false,
+        uploadingFile: false,
         directory: {},
         files: [],
+        fileToDelete: '',
+        deleteMsg: false,
+        snackbar: false,
+        snackbarColor: 'primary',
+        snackbarText: '',
       }
     },
     computed: {
@@ -126,13 +185,37 @@
         }
         return 'mdi-file'
       },
+      uploadFile (event) {
+        this.uploadingFile = true
+        const formData = new FormData()
+        formData.append('file', event.target.files[0])
+        axios.post(
+          'companies/' + this.$route.params.id + '/documents/' + this.directory.code + '/upload',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          },
+        ).then(res => {
+          this.uploadingFile = false
+          this.getFiles()
+        })
+      },
+      deleteFile () {
+        axios.delete('companies/' + this.$route.params.id + '/documents/' + this.directory.code + '/' + this.fileToDelete + '/destroy')
+          .then(res => {
+            this.snackbar = true
+            this.snackbarText = res.data.message
+            this.getFiles()
+          })
+        this.deleteMsg = false
+      },
     },
   }
 </script>
 
 <style lang="sass">
-.doc-expansion-header
-  font-size: 16px !important
-.v-expansion-panel-content
-  padding-left: 20px
+.file-upload
+  width: 100%
 </style>
