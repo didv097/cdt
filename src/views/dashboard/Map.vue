ada6a19cb21f129072cf5a0d4cb3a10fbcf64623
bran0 <bran107@outlook.com>
2020-02-23 22:09:47 -0500

    cop map

--- src/views/dashboard/Map.vue
+++ src/views/dashboard/Map.vue
@@ -1,31 +1,50 @@
 <template>
-  <v-container
-    id="map"
-    tag="section"
-  >
-    <!-- <l-map>
-      <l-tile-layer></l-tile-layer>
-    </l-map> -->
-  </v-container>
+  <div class="cop-map-container">
+    <l-map
+      id="map"
+      ref="map"
+      :zoom="3"
+      :min-zoom="3"
+      :world-copy-jump="true"
+      :options="mapOptions"
+    >
+      <l-control-zoom position="topright" />
+      <l-control-layers
+        ref="layerControl"
+        position="topright"
+      />
+      <l-control-scale
+        :imperial="true"
+        :metric="true"
+        position="bottomright"
+      />
+      <l-tile-layer
+        url="https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFucnVsczEiLCJhIjoiY2swcGlhNXpwMDBzNTNvcmcwZDQxcDB6cyJ9.w3WTQktdZ2nMziZtXNoRKQ"
+      />
+      <l-layer-group>
+        <v-marker-cluster ref="vesselMarkers" />
+      </l-layer-group>
+    </l-map>
+  </div>
 </template>
 
 <script>
+  import { LMap, LTileLayer, LControlZoom, LControlLayers, LControlScale, LLayerGroup } from 'vue2-leaflet'
 
   export default {
-    name: 'Map',
-
-    components: {
-    },
+    components: { LMap, LTileLayer, LControlZoom, LControlLayers, LControlScale, LLayerGroup },
     data: () => ({
-      search: '',
-      options: {},
+      mapOptions: {
+        attributionControl: false,
+        zoomControl: false,
+        updateWhenZooming: false,
+        updateWhenIdle: false,
+        touchZoom: true,
+      },
     }),
-    async mounted () {
-      await this.getDataFromApi()
+    mounted () {
     },
     methods: {
-      async getDataFromApi () {
-      },
     },
   }
 </script>
